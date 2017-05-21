a6562f6d2a112577ce3412b3c7447aa0aa16714b
nathan <grimaudathan@gmail.com>
2017-05-21 19:20:39 +0200

    added log4j

--- src/fr/epsi/myEpsi/servlet/Users.java
+++ src/fr/epsi/myEpsi/servlet/Users.java
@@ -2,19 +2,17 @@ package fr.epsi.myEpsi.servlet;
 
 import java.io.IOException;
 import java.util.List;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 import fr.epsi.myEpsi.beans.User;
-import fr.epsi.myEpsi.dao.IUserDao;
-import fr.epsi.myEpsi.dao.UserDao;
 import fr.epsi.myEpsi.service.IUserService;
 import fr.epsi.myEpsi.service.UserService;
-import org.apache.log4j.*;
+
 /**
  * Servlet implementation class Users
  */
@@ -23,8 +21,7 @@ import org.apache.log4j.*;
 public class Users extends HttpServlet {
 	
 	IUserService service = new UserService();
-	Logger logger =  Logger.getLogger(Users.class);
-	
+	Logger logger =  LogManager.getLogger(Users.class.getName());
 	private static final long serialVersionUID = 1L;
        
     /**
