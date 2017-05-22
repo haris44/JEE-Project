f7e4da9bfee6c803e02fa5e3e488c90934e5f362
Bastien Balaud <bastien.balaud@labo.loc>
2017-05-22 14:55:40 +0200

    Bastien JXM corde

--- src/fr/epsi/myEpsi/jmx/Premier.java
+++ src/fr/epsi/myEpsi/jmx/Premier.java
@@ -1,5 +1,14 @@
 package fr.epsi.myEpsi.jmx;
 
+import java.security.Timestamp;
+import java.util.Date;
+
+import fr.epsi.myEpsi.beans.Message;
+import fr.epsi.myEpsi.beans.Status;
+import fr.epsi.myEpsi.service.IMessageService;
+import fr.epsi.myEpsi.service.MessageService;
+import fr.epsi.myEpsi.service.UserService;
+
 public class Premier implements PremierMBean {
 
 	private static String name = "Premier MBEAN";
@@ -24,6 +33,27 @@ public class Premier implements PremierMBean {
 	public void refresh() {
 		System.out.println("Refresh");
 		
+	}
+	@Override
+	public Integer getNumberFfMessage()
+	{
+		IMessageService messageService = new MessageService();
+    	Integer messagesSize = messageService.getListOfMessages().size();
+    	return messagesSize;
+	}
+	@Override 
+	public void pushAdminMessage(String messageContenu)
+	{
+		MessageService messageService = new MessageService();
+		UserService userService = new UserService();
+		Message monMessage = new Message();
+		monMessage.setAuthor(userService.getUserById("ADMIN"));
+		monMessage.setStatus(Status.PUBLIC);
+		monMessage.setCreationDate(new java.sql.Timestamp(new Date().getTime()));
+		monMessage.setTitle("Admin Broadcast");
+		monMessage.setContent(messageContenu);
+		messageService.addMessage(monMessage);
+	
 	}
 
 }
